14e2405751cfe9d0a5caa6eeae04a7f299de45d2
Xayrulloh <xayrullohabduvohidov713@gmail.com>
2025-01-24 20:26:51 +0500

    feat: fcm-token controller added; feat: swagger with zod implementation also added; refactor: guards must be fixed

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,10 +4,31 @@ import { UsersModule } from './modules/users/users.module';
 import { DrizzleModule } from './database/drizzle.module';
 import { EnvModule } from './config/env/env.module';
 import { CookiesModule } from './config/cookies/cookies.module';
+import { FCMTokenModule } from './modules/fcm-token/fcm-token.module';
+import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import { ZodSerializerInterceptor, ZodValidationPipe } from 'nestjs-zod';
+import { HttpExceptionFilter } from './common/filters/http.filter';
 
 @Module({
-  imports: [AuthModule, UsersModule, DrizzleModule, EnvModule, CookiesModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    DrizzleModule,
+    EnvModule,
+    CookiesModule,
+    FCMTokenModule,
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useClass: ZodValidationPipe,
+    },
+    { provide: APP_INTERCEPTOR, useClass: ZodSerializerInterceptor },
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter
+    }
+  ],
 })
 export class AppModule {}
---
+++ apps/api/src/modules/fcm-token/dto/fcm-token.dto.ts
@@ -0,0 +1,8 @@
+import { FCMTokenSchema } from "@family-tree/shared";
+import { createZodDto } from 'nestjs-zod'
+
+class FCMTokenCreateDeleteRequestDto extends createZodDto(FCMTokenSchema.pick({ token: true, deviceType: true })) {}
+
+class FCMTokenResponseDto extends createZodDto(FCMTokenSchema) {}
+
+export { FCMTokenCreateDeleteRequestDto, FCMTokenResponseDto }
